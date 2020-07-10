462890626e5b5e47636e8a2394835b5ce2382aa1
hamedr <hamedr@sfu.ca>
2020-07-10 16:57:50 -0700

    little change

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-"""General-purpose test script for image-to-image translation.
+"""General-purpose test script for image-to-image translation hmm.
 
 Once you have trained your model with train.py, you can use this script to test the model.
 It will load a saved model from --checkpoints_dir and save the results to --results_dir.
