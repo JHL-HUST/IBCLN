620549e558cdd3b0ce5bf0dd7394791b6e2364e9
Aditya Shukla <adityashukzy@gmail.com>
2022-02-15 20:27:31 +0530

    Refactor deprecated scipy function to cv2 function

--- util/visualizer.py
+++ util/visualizer.py
@@ -5,7 +5,7 @@ import ntpath
 import time
 from . import util, html
 from subprocess import Popen, PIPE
-from scipy.misc import imresize
+from cv2 import resize
 
 if sys.version_info[0] == 2:
     VisdomExceptionBase = Exception
@@ -41,9 +41,9 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
             save_path = os.path.join(image_dir, image_name)
             h, w, _ = im.shape
             if aspect_ratio > 1.0:
-                im = imresize(im, (h, int(w * aspect_ratio)), interp='bicubic')
+                im = resize(im, (h, int(w * aspect_ratio)), interpolation='bicubic')
             if aspect_ratio < 1.0:
-                im = imresize(im, (int(h / aspect_ratio), w), interp='bicubic')
+                im = resize(im, (int(h / aspect_ratio), w), interpolation='bicubic')
             util.save_image(im, save_path)
 
             ims.append(image_name)
